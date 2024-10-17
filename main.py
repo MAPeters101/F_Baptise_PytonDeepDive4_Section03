5aa2b392fadab5889610e277ad6df01f2bcb7331
Mark Peters <mapeters101@earthlink.net>
2024-10-17 14:44:28 -0400

    Lesson037:  Complete work on TimeZone from 7:30.

--- main.py
+++ main.py
@@ -20,9 +20,45 @@ class TimeZone:
         if offset < timedelta(hours=-12, minutes=0) or offset > timedelta(hours=14, minutes=0):
             raise ValueError('Offset must be between -12:00 and +14:00.')
 
+        self._offset_hours = offset_hours
+        self._offset_minutes = offset_minutes
+        self._offset = offset
 
 
+    @property
+    def offset(self):
+        return self._offset
+
+    @property
+    def name(self):
+        return self._name
+
+    def __eq__(self, other):
+        return (isinstance(other, TimeZone) and
+                self.name == other.name and
+                self._offset_hours == other._offset_hours and
+                self._offset_minutes == other._offset_minutes)
+
+    def __repr__(self):
+        return (f"TimeZone(name='{self.name}', "
+                f"offset_hours={self._offset_hours}, "
+                f"offset_minutes={self._offset_minutes})")
 
 
+tz1 = TimeZone('ABC', -2, -15)
+print(tz1.name)
 
+from datetime import datetime
+dt = datetime.utcnow()
+print(dt)
+print(dt + tz1.offset)
 
+try:
+    tz = TimeZone('', 0, 0)
+except ValueError as ex:
+    print(ex)
+
+try:
+    tz = TimeZone('ABC', 18, 0)
+except ValueError as ex:
+    print(ex)
