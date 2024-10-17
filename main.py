5f10e28d6660919f9deeab01627303ef08174827
Mark Peters <mapeters101@earthlink.net>
2024-10-17 13:55:24 -0400

    Lesson037:  Work on TimeZone up to 7:30.

---
+++ main.py
@@ -0,0 +1,28 @@
+import numbers
+from datetime import timedelta
+
+class TimeZone:
+    def __init__(self, name, offset_hours, offset_minutes):
+        if name is None or len(str(name).strip()) == 0:
+            raise ValueError('Timezone name cannot be empty.')
+        self._name = str(name).strip()
+
+        if not isinstance(offset_hours, numbers.Integral):
+            raise ValueError('Hour offset must be an integer.')
+
+        if not isinstance(offset_minutes, numbers.Integral):
+            raise ValueError('Minute offset must be an integer.')
+
+        if offset_minutes > 59 or offset_minutes < -59:
+            raise ValueError('Minute offset must be between -59 and 59 (inclusive).')
+
+        offset = timedelta(hours=offset_hours, minutes=offset_minutes)
+        if offset < timedelta(hours=-12, minutes=0) or offset > timedelta(hours=14, minutes=0):
+            raise ValueError('Offset must be between -12:00 and +14:00.')
+
+
+
+
+
+
+
